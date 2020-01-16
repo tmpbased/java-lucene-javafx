087dc6cf927fe1d17bb140f1ca4dd2b559cc6eb8
zVcXr <zvcxr666@gmail.com>
2020-01-17 01:52:10 +0200

    9

--- tilt-test/tilt-lib-test/src/test/java/tilt/lib/receiver/v2/A.java
+++ tilt-test/tilt-lib-test/src/test/java/tilt/lib/receiver/v2/A.java
@@ -1,13 +1,13 @@
 package tilt.lib.receiver.v2;
 
 import tilt.apt.dispatch.annotations.Case;
+import tilt.apt.dispatch.annotations.Switch;
 
 public abstract class A<T> implements Comparable<T> {
   // TODO generate nothing if there is no Switch annotation.
-  abstract void testSwitch(Number value);
+  abstract void testSwitch(@Switch Number value);
 
-  void testCaseInteger(@Case Integer value) {
-  }
+  void testCaseInteger(@Case Integer value) {}
 
   @Override
   public int compareTo(T o) {
--- tilt-test/tilt-lib-test/src/test/java/tilt/lib/receiver/v2/C.java
+++ tilt-test/tilt-lib-test/src/test/java/tilt/lib/receiver/v2/C.java
@@ -1,15 +1,18 @@
 package tilt.lib.receiver.v2;
 
 import tilt.apt.dispatch.annotations.Case;
+import tilt.apt.dispatch.annotations.Switch;
 
 public abstract class C<X> extends C_GeneratedSuperclass<X, A<X>> {
-  public C() {
-  }
+  public C() {}
 
   public C(Number value) throws Exception { // TODO newInstance(value)
   }
 
-  void testCaseDouble(@Case Double value) /** TODO throws java.io.IOException */
-  {
-  }
+  @Override
+  abstract void testSwitch(@Switch Number value);
+
+  void testCaseDouble(@Case Double value)
+        /** TODO throws java.io.IOException */
+      {}
 }
