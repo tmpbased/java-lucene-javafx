db3ae3adb9b2e99d8516b3a5cbcef285b8cdd0c3
zVcXr <zvcxr666@gmail.com>
2020-01-19 20:43:28 +0200

    11

--- tilt-test/tilt-lib-test/src/test/java/tilt/lib/receiver/v2/C.java
+++ tilt-test/tilt-lib-test/src/test/java/tilt/lib/receiver/v2/C.java
@@ -1,18 +1,24 @@
 package tilt.lib.receiver.v2;
 
+import java.io.Serializable;
+import java.util.List;
 import tilt.apt.dispatch.annotations.Case;
 import tilt.apt.dispatch.annotations.Switch;
 
-public abstract class C<X/** TODO extends A<X> */> extends C_GeneratedSuperclass<X, A<X>> {
-  public C() {
-  }
+public abstract class C<
+        X extends List<C<X>> & Serializable
+        /** TODO extends A<X> */
+        >
+    extends C_GeneratedSuperclass<X, A<X>> {
+  public C() {}
 
-  protected C(Number defaultValue) throws Exception {
-  }
+  protected <Q extends X, Z extends C<? super List<Number>> & Serializable> C(Z defaultValue)
+      throws Exception {}
 
   @Override
   abstract void testSwitch(@Switch Number value);
 
-  void testCaseDouble(@Case Double value) /** TODO throws java.io.IOException */ {
-  }
+  void testCaseDouble(@Case Double value)
+        /** TODO throws java.io.IOException */
+      {}
 }
