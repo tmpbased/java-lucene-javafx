82348437a97297af9a81fa01897918bc22a36574
zVcXr <zvcxr666@gmail.com>
2020-01-21 19:41:57 +0200

    13

--- tilt-test/tilt-lib-test/src/test/java/tilt/lib/receiver/v2/A.java
+++ tilt-test/tilt-lib-test/src/test/java/tilt/lib/receiver/v2/A.java
@@ -1,10 +1,12 @@
 package tilt.lib.receiver.v2;
 
+import java.io.IOException;
+
 import tilt.apt.dispatch.annotations.Case;
 import tilt.apt.dispatch.annotations.Switch;
 
 public abstract class A<T> implements Comparable<T> {
-  abstract void testSwitch(@Switch Number value);
+  abstract void testSwitch(@Switch Number value) throws IOException;
 
   void testCaseInteger(@Case Integer value) {}
 
